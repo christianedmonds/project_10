5f4ef49efc0bf8c5610b90833b874a1b5ff7391c
christian edmonds <christianedmonds@gmail.com>
2017-04-03 12:37:19 -0700

    API integration
    integrated API info with structure

--- js/master.js
+++ js/master.js
@@ -3,10 +3,12 @@ var spotOptions = {} ;
 function displayAlbum(data) {
   var albumHTML = "";
   $.each(data.items, function (i, album) {
-    albumHTML += "<a>";
-    albumHTML += ""
+    albumHTML += '<a class="cover" href="' + album.images[0].url + '"';
+    albumHTML += 'data-lightbox="gallery"';
+    albumHTML += 'data-title="' + album.artists[0].name + ' // ' +  album.name +' // ' + album.album_type +'">';
+    albumHTML += '<img src=" ' + album.images[1].url + ' "></img>';
+    albumHTML += '</a>';
   });
-};
-
-
+  $(".album_wrapper").html(albumHTML);
+}
 $.getJSON(spotAPI, spotOptions, displayAlbum);
